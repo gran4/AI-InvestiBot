be74341c91176e4ff2f7e20ddf29c137ee693927
Aidan Catchpool <acatchpool@protonmail.com>
2023-06-26 11:15:50 +0100

    Added docstrings to two modules.

--- trading_system.py
+++ trading_system.py
@@ -1,9 +1,33 @@
+"""
+Name:
+    trading_system.py
+
+Purpose:
+    This module provides a trading system that can be used to trade stocks.
+
+Author:
+    Grant Yul Hur
+
+See also:
+    Other modules related to running the stock bot -> lambda_implementation, loop_implementation
+"""
+
 from math import floor
 from Models import MACDModel
 from Tradingfuncs import company_symbols
 
 
 class DayTrader():
+    """
+    This is the base class for the DayTrader. Currently it only initalises
+    a hard coded start date, end date, and stock symbol. It will be used to
+    trade stocks.
+
+    Args:
+        start_date (str): The start date of the trading system
+        end_date (str): The end date of the trading system
+        stock_symbol (str): The stock symbol of the stock you want to trade
+    """
     def __init__(self, start_date: str = "2020-01-01",
                  end_date: str = "2023-06-09",
                  stock_symbol: str = "AAPL") -> None:
@@ -16,6 +40,18 @@ NOTE: Change to use amount of stock not money in stock.
 all_models = {}#???????????? USE?????
 holdings = {}
 def update_all(model, manager):
+    """
+    This function will be used to update all the models and holdings.
+    It can be used to create predictions and sort them. From these
+    predictions it could determine whether it should buy or sell the stocks.
+
+    Args:
+        model (Any): The model that will be used to predict the stock prices
+        manager (Any): The manager that will be used to buy and sell stocks
+
+    Returns:
+        None
+    """
     #predicts tommorrows prices and sorts it
     predictions = {}
     for company_ticker in company_symbols:
