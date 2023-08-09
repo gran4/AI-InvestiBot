453e98f4435b82b9b3cc0c26aaf38a64f78b54a2
Grant Hur <fifttim@gmail.com>
2023-08-08 17:40:18 -0700

    Add Transfer Learning

---
+++ Stock-Bot-Predicter-AI/from_project.py
@@ -0,0 +1,15 @@
+import json
+from tensorflow.keras.models import load_model
+from trading_funcs import company_symbols
+
+
+def get_years():
+    lis = []
+    for company in company_symbols:
+        with open(f'Stocks/{company}/dynamic_tuning.json', 'r') as file:
+            dynamic_tuning = json.load(file)
+        lis.append((dynamic_tuning['relevant_years'], company))
+    lis = sorted(lis, key=lambda x: x[0])
+
+def get_transfer_learning_model():
+    return load_model(f"transfer_learning_model")
